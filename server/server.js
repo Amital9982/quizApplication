f484f76347fb38676241ce3b5c01ffca37f8f04c
= <mayankrathore9897@gmail.com>
2024-08-09 17:14:12 +0530

    by using mongodb atlas

--- server/server.js
+++ server/server.js
@@ -8,7 +8,6 @@ const usersRoute = require("./routes/usersRoute");
 const examsRoute = require("./routes/examsRoute");
 const resportsRoute = require("./routes/reportsRoute");
 
-
 app.use("/api/users", usersRoute);
 app.use("/api/exams", examsRoute);
 app.use("/api/reports", resportsRoute);
@@ -18,12 +17,11 @@ const path = require("path");
 __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client" , "build")));
+  app.use(express.static(path.join(__dirname, "client", "build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });   
-} 
-
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
